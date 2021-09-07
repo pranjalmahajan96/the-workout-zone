5765b1bf1a78b0f6571b7b92e3cb02127dfae4f8
Pranjal Mahajan <pranjalmahajan96@gmail.com>
2021-09-07 20:26:44 +0530

    on-login navigation

--- src/features/user/login/Login.jsx
+++ src/features/user/login/Login.jsx
@@ -1,11 +1,18 @@
+import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
 import { logoutBtnPressed } from "../userSlice";
 import { LoginForm } from "./LoginForm";
 
 export const Login = () => {
+  const navigate = useNavigate();
   const dispatch = useDispatch();
   const login = useSelector((state) => state.user.loginStatus);
   const name = useSelector((state) => state.user.user.name);
+
+  useEffect(()=>{
+    login ? navigate("/") : navigate("/login")
+  },[login, navigate])
 
   return login ? (
     <>
