991b3e6d1bb99bc0f294b584a762c3b17cd51b09
Pranjal Mahajan <pranjalmahajan96@gmail.com>
2021-09-07 20:12:33 +0530

    header

--- src/features/header/Header.jsx
+++ src/features/header/Header.jsx
@@ -7,29 +7,51 @@ export const Header = () => {
   const login = useSelector(state=>state.user.loginStatus)
   return (
     <div>
+      {
+        login ?  <div>
+        <nav className="navigation">
+          <div className="nav-brand">
+            <NavLink to="/" className="link">
+              The Workout Zone
+            </NavLink>
+          </div>
+          <ul className="list-non-bullet nav-pills">
+            <li className="list-item-inline">
+              <NavLink to="/likedvideos" className="link link-active">
+                Liked Videos
+              </NavLink>
+            </li>
+            <li className="list-item-inline">
+              <NavLink to="/watchlater" className="link link-active">
+                Watch Later
+              </NavLink>
+            </li>
+            {/* <li className="list-item-inline">
+              <NavLink to="/history" className="link link-active">
+                History
+              </NavLink>
+            </li> */}
+            <li className="list-item-inline">
+              <NavLink to="/login" className="link link-active">
+               { login ?  
+               <button
+                className="btn btn-outline btn-no-hover btn-header"
+                onClick={() => dispatch(logoutBtnPressed())}
+              > Logout  </button> 
+              : "Login" }
+              </NavLink>
+            </li>
+          </ul>
+        </nav>
+      </div> 
+      :  <div>
       <nav className="navigation">
         <div className="nav-brand">
           <NavLink to="/" className="link">
             The Workout Zone
           </NavLink>
         </div>
-
         <ul className="list-non-bullet nav-pills">
-          <li className="list-item-inline">
-            <NavLink to="/likedvideos" className="link link-active">
-              Liked Videos
-            </NavLink>
-          </li>
-          <li className="list-item-inline">
-            <NavLink to="/watchlater" className="link link-active">
-              Watch Later
-            </NavLink>
-          </li>
-          <li className="list-item-inline">
-            <NavLink to="/history" className="link link-active">
-              History
-            </NavLink>
-          </li>
           <li className="list-item-inline">
             <NavLink to="/login" className="link link-active">
              { login ?  
@@ -43,5 +65,8 @@ export const Header = () => {
         </ul>
       </nav>
     </div>
+      }
+    </div>
+   
   );
 };
